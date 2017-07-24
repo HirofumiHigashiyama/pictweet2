964d4392da16dc78028170e7787af9846e330a80
Hirofumi Higashiyama <hirofumihigashiyama@pc1.local>
2017-07-24 12:48:02 +0900

    Tweet.create(tweet_params)にストロングパラメーターをprivateメソッドを用いて設定した。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,9 @@ class TweetsController < ApplicationController
   def create
     Tweet.create(name: params[:name], text: params[:text], image: params[:image])
   end
+
+  private
+  def tweet_params
+    params.permit(:name, :text, :image)
+  end
 end
